b4c6c1d8256f290d7ab9ea666f04ead58a8ae5ff
Mike Leonard <mtleonardjr@gmail.com>
2021-02-24 19:02:49 -0500

    added controller functions

--- src/library/Controller.js
+++ src/library/Controller.js
@@ -1,12 +1,12 @@
 class Controller {
 
     cycle(tileData){ 
-        const temp = [...tileData];
+        // let temp = [...tileData];
 
-        // loop thru our array & perform an action
-        temp.forEach((e)=>{
-            e.num ++;
-        })
+        // // loop thru our array & perform an action
+        // temp.forEach((e)=>{
+        //     e.num ++;
+        // })
 
         // // Target an individual tile & perform an action
         // temp[0] = {...temp, num:5};
@@ -14,7 +14,34 @@ class Controller {
         // logging statement
         // console.log(temp);
 
-        return(temp)
+        const createTile = () => {
+            let tile = {
+                surface: {
+                    surfaceType: "land",
+                    fertility: .5
+                },
+                atmosphere: {
+                    oxygen: .5,
+                    nitrogen: .5
+                },
+                uGround: {
+                    waterSat: .75
+                }
+            }
+            return tile;
+        }
+
+        const getTiles = (num) => {
+            let tileArray = [];
+            for (let i=0;i<num;i++){
+                tileArray.push(createTile());
+            }
+            return tileArray;
+        }
+
+        
+
+        return(getTiles(4))
     }
 }
 
