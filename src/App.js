5f102b780932f6ef0e5d5abf33fbd9c1aa5eb927
Mike Leonard <mtleonardjr@gmail.com>
2021-03-18 12:01:01 -0400

    built toggle function/reworked functions

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import Tile from './components/Tile'
 import Info from './components/Info'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 const Controller = require('./library/controller')
 const Utilities = require('./library/utilities')
 
@@ -9,9 +9,10 @@ function App() {
 
   const initialArray = Utilities.createInitalArray(15)
 
+  const [seconds, setSeconds] = useState(0);
   const [tileData, setTileData] = useState(initialArray) 
   const [infoData, setInfoData] = useState({position:{xPos:0,yPos:0},"surface":{tectonicPlate: 1,"surfaceType":"land","fertility":0.5},"atmosphere":{"oxygen":0.5,"nitrogen":0.5},"uGround":{"waterSat":0.75}}) 
-  const [gameData, setGameData] = useState({cycle: 0, worldName: "Test World", selected: [99], cycleToggle: 0})
+  const [gameData, setGameData] = useState({cycle: 0, worldName: "Test World", selected: [99], cycleToggle: false})
 
   //Cycle Method
   const cycle = () => {
@@ -35,12 +36,28 @@ function App() {
 
   //Toggle Cycle
   const toggleCycle = () => {
-    if(gameData.cycle < 10) {
-      setInterval(()=>{ 
-        cycle();
-      }, 1000);
-    }
+    // const temp = gameData;
+    // temp.cycleToggle = !temp.cycleToggle;
+    // console.log(gameData.cycleToggle)
+    // setGameData(temp);
+
+    setGameData({...gameData, cycleToggle: !gameData.cycleToggle})
   }
+
+
+
+
+  // //Auto Cycle
+  // const autoCycle = () => {
+  //   let interval = null;
+  //   if(gameData.cycleToggle === true) {
+  //     interval = setInterval(()=>{ 
+  //       cycle();
+  //     }, 1000);
+  //   } else {
+  //     clearInterval(interval);
+  //   }
+  // }
 
   // get tile data from hovered tile
   const getHoverData = (data) => {
@@ -51,17 +68,37 @@ function App() {
 
   //increase cycle count
   const upCycle = () => {
-    const temp = gameData;
-    temp.cycle++;
-    setGameData(temp)
+    // const temp = gameData;
+    // temp.cycle++;
+    // setGameData(temp)
+
+    setGameData({...gameData, cycle: gameData.cycle+1})
   }
 
   //reset cycle count
   const resetCycle = () => {
-    const temp = gameData;
-    temp.cycle= 0;
-    setGameData(temp)
+    // const temp = gameData;
+    // temp.cycle= 0;
+    // setGameData(temp);
+
+    setGameData({...gameData, cycle: 0})
   }
+
+  useEffect(() => {
+    console.log("test");
+    
+    let interval = null;
+    if (gameData.cycleToggle) {
+      console.log("test 2");
+      interval = setInterval(()=>{ 
+        cycle();
+      }, 1000);
+    } else {
+      clearInterval(interval);
+    }
+
+    return () => clearInterval(interval);
+  }, [gameData])
 
   return (
     <div className="app-container">
